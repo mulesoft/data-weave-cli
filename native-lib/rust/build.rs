cb2db5f430273a9647eb9061e56970bca36be186
Martin Cousido <mcousido@salesforce.com>
2026-06-17 18:01:54 -0300

    feat(native-lib): implement Rust FFI bindings for run_script

---
+++ native-lib/rust/build.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main() {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let lib_dir = format!("{}/../../build/native/nativeCompile", manifest_dir);
+
+    println!("cargo:rustc-link-search=native={}", lib_dir);
+    println!("cargo:rustc-link-lib=dylib=dwlib");
+
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_dir);
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_dir);
+}
